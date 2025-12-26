2d868e22162ee06a84fee9b2266403464b413934
AlcMaple <2933724627@qq.com>
2025-12-26 09:57:02 +0800

    Chore: 日常维护

--- apis/base/api/__init__.py
+++ apis/base/api/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-router_simple = APIRouter(prefix="/api", tags=["简单路由模块"])
+router_simple = APIRouter(prefix="/api", tags=["简单模块"])
 router_user = APIRouter(prefix="/api", tags=["用户模块"])
 
 from . import simple
